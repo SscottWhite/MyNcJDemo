4868ce5aaf1906efb0a81f66810c57a0aca35ba6
god <18762988789@163.com>
2020-11-13 16:59:34 +0800

    invoke

---
+++ src/main/java/com/example/main/test/MainInvokeTest.java
@@ -0,0 +1,12 @@
+package com.example.main.test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainInvokeTest {
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+		Method m = String.class.getMethod("substring", int.class);
+		String s = (String) m.invoke("ABCDEFGHIJK", 6);
+		System.out.print(s);
+	}
+}
