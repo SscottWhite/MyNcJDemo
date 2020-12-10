b98dbc9e0922c10f713a51a3e6b47117f7b9efd1
god <18762988789@163.com>
2020-12-10 16:50:21 +0800

    json test

---
+++ src/main/java/com/example/jsontype/test/JsonTest.java
@@ -0,0 +1,11 @@
+package com.example.jsontype.test;
+
+import com.alibaba.fastjson.JSONObject;
+
+public class JsonTest {
+	
+	public static void main(String[] args) {
+		String jonStr = "{\"name\":\"vivin\",\"age\":20, \"hoppy\":[\"read\",\"write\"]}";
+		JSONObject json = new JSONObject();
+	}
+}
