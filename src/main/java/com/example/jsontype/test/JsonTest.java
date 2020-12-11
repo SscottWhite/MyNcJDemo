400654817d13fe5f6f494bdc4f1e8a5b85b6e1be
god <18762988789@163.com>
2020-12-11 14:48:14 +0800

    up json

--- src/main/java/com/example/jsontype/test/JsonTest.java
+++ src/main/java/com/example/jsontype/test/JsonTest.java
@@ -1,11 +1,17 @@
 package com.example.jsontype.test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 public class JsonTest {
 	
 	public static void main(String[] args) {
 		String jonStr = "{\"name\":\"vivin\",\"age\":20, \"hoppy\":[\"read\",\"write\"]}";
-		JSONObject json = new JSONObject();
+		JSONObject jsonobject = new JSONObject();
+		JSONArray  jsonArray = new JSONArray();
+
+		jsonobject = JSON.parseObject(jonStr);
+		System.out.print(jsonobject.getJSONArray("hoppy"));
 	}
 }
