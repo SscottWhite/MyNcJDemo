f60d49d6fff98a496393ed30063e8d8bf90548ce
god <18762988789@163.com>
2020-12-15 15:10:33 +0800

    update

--- src/main/java/com/example/IO/Impl/ExcelTest.java
+++ src/main/java/com/example/IO/Impl/ExcelTest.java
@@ -1,8 +1,14 @@
-package com.example.excel.test;
+package com.example.IO.Impl;
 
 import com.spire.xls.FileFormat;
 import com.spire.xls.Workbook;
 
+
+/**
+ *  用了第三方的jdk,直接转换
+ * @author KJS_352
+ *
+ */
 public class ExcelTest {
 	public static void main(String[] args) {
         //加载Excel文档
