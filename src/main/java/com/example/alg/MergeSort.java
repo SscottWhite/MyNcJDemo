5c49ddde2e9f919f1040ea4b93b4ea18e4159976
god <18762988789@163.com>
2020-12-23 15:06:20 +0800

    归并排序

--- src/main/java/com/example/alg/MergeSort.java
+++ src/main/java/com/example/alg/MergeSort.java
@@ -2,21 +2,54 @@ package com.example.alg;
 
 /**
  * s 归并排序
+ * ss 很有趣的递归算法, 先分两头逐布拆分, 在分布逐步聚合, 最后两个相溶, 
  * @author KJS_352
  *
  */
 public class MergeSort {
 	public static void main(String[] args) {
 		int[] a = new int[] {1,6,3,2,8,5,7,4};
-		int[] b = sort(a);
+		int[] b = new int[a.length];
+		sort(a,b,0,a.length-1);
 		//int[] b = sort2(a,0,a.length-1);
 		for(int x = 0; x < b.length; x++) {
 			System.out.println(b[x]);			
 		}
 	}
 	
-	public static int[] sort(int[] array) {
+	public static int[] sort(int[] array,int[] tmp ,int start, int end) {
 		
+		if(start < end) {
+			 int mid = (end + start) / 2;
+			 sort(array,tmp,start,mid);
+			 sort(array,tmp,mid+1,end);
+			 marge(array, tmp, start, mid, mid+1,end);//这个参数分两组, [  start - mid ]   ,[ mid+1 - end ]
+		}
 		return array;
+	}
+	
+	public static void marge(int[] array,int[] temp, int head,int headmid,int footmid, int foot) {
+		int len = foot - head + 1;//数组总长度
+		int count = head;//计数器从头开始
+		while(head <= headmid && footmid <= foot) {//两组分别开始
+			if(array[head] < array[footmid]) {
+				temp[count++] = array[head++];
+			}else {
+				temp[count++] = array[footmid++];
+			}
+		}
+		//剩下的都直接接到后面
+		while(head <= headmid) {
+			temp[count++] = array[head++];
+		}
+		
+		while(footmid <= foot) {
+			temp[count++] = array[footmid++];
+		}
+		//把排好序的还给它
+		for(int i = 0; i < len; i++, foot--) {
+			array[foot] = temp[foot];
+		}
+		
 	}
 }
--- src/main/java/com/example/alg/SortTest.java
+++ src/main/java/com/example/alg/SortTest.java
@@ -20,7 +20,7 @@ public class SortTest {
 		System.out.print(j+","+a.length);
 	}*/
 	
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		int i = 1;
         i = i++;//这一步没自增
         int j = i++;
@@ -28,5 +28,35 @@ public class SortTest {
         System.out.println("i="+i);
         System.out.println("j="+j);
         System.out.println("k="+k);
+	}*/
+	
+	public static void main(String[] args) {	
+		int[] a = new int[] {1,6,3,2,8,5,7,4,9,20,33,32,23,12,11};
+		int[] b = new int[a.length];
+		
+		long startTime = System.currentTimeMillis();
+	BubbleSort.sort(a);
+		long endTime = System.currentTimeMillis();
+		System.out.println("冒泡:"+(endTime-startTime));
+		long startTime1 = System.currentTimeMillis();
+	DirectInsertSort.sort(a);
+		long endTime1 = System.currentTimeMillis();
+		System.out.println("插入:"+(endTime1-startTime1));
+		long startTime2 = System.currentTimeMillis();
+	MergeSort.sort(a,b,0,a.length-1);
+		long endTime2 = System.currentTimeMillis();
+		System.out.println("归并:"+(endTime2-startTime2));
+		long startTime3 = System.currentTimeMillis();
+	SelectSort.sort(a);
+		long endTime3 = System.currentTimeMillis();
+		System.out.println("选择:"+(endTime3-startTime3));
+		long startTime4 = System.currentTimeMillis();
+	ShellSort.sort(a);
+		long endTime4 = System.currentTimeMillis();
+		System.out.println("希尔:"+(endTime4-startTime4));
+		long startTime5 = System.currentTimeMillis();
+	QuickSort.sort(a,0,a.length-1);
+		long endTime5 = System.currentTimeMillis();
+		System.out.println("快速:"+(endTime5-startTime5));
 	}
 }
