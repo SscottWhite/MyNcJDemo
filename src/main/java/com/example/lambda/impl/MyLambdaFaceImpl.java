1257663527998776b43c86eabe97d96112a24f2f
god <18762988789@163.com>
2021-01-13 16:52:20 +0800

    up

--- src/main/java/com/example/lambda/impl/MyLambdaFaceImpl.java
+++ src/main/java/com/example/lambda/impl/MyLambdaFaceImpl.java
@@ -26,7 +26,7 @@ public class MyLambdaFaceImpl {
 		//faceImpl4.face4("tsetface",String -> {System.out.println(String)});
 		faceImple.face4("tsetfase",System.out::println);//相当于利用lambda简单重写了方法
 		
-		//在省去重写部分的参数, 也就是着重 重写方法的参数和返回值要和接口相同
+		//在省去重写部分的参数, 也就是着重 重写方法的参数和返回值要和接口相同  直接放了最简便的方法体部分
 		//记住是引用的方法返回值和接口的返回值相同, 应用了重写的概念, 实现的最终就是fase5方法
 		MyLambdaFace2 face22 = new FanXing()::face5;  // => String -> {new Fanxing().face5(String)}
 		System.out.println(face22.run("test22"));
