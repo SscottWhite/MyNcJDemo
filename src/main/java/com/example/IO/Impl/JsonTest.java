3f4af30fd7790f6b30a53624657837938bf3e821
NC <179813964@qq.com>
2021-01-15 22:26:40 +0800

    test 22.26

--- src/main/java/com/example/IO/Impl/JsonTest.java
+++ src/main/java/com/example/IO/Impl/JsonTest.java
@@ -3,9 +3,9 @@ package com.example.IO.Impl;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-
 public class JsonTest {
-	
+
+
 	public static void main(String[] args) {
 		String jonStr = "{\"name\":\"vivin\",\"age\":20, \"hoppy\":[\"read\",\"write\"]}";
 		JSONObject jsonobject = new JSONObject();
