ea4750215d524a799c0b1193b8a346f125f19a96
NC <179813964@qq.com>
2021-01-17 11:23:02 +0800

    test 1.17

--- src/main/java/com/example/IO/Impl/XmlBuiler.java
+++ src/main/java/com/example/IO/Impl/XmlBuiler.java
@@ -13,7 +13,8 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
