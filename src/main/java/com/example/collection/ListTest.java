4541bf7f7f0ad1898ce35e10b88f5fa2e0c2ebc1
god <18762988789@163.com>
2021-02-22 16:54:32 +0800

    up  2.22 3

--- src/main/java/com/example/collection/ListTest.java
+++ src/main/java/com/example/collection/ListTest.java
@@ -1,10 +1,14 @@
 package com.example.collection;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+
 
 public class ListTest {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
+		Collection collections = new ArrayList();
 	}
 }
--- src/main/java/com/example/collection/MapTest.java
+++ src/main/java/com/example/collection/MapTest.java
@@ -1,19 +1,28 @@
 package com.example.collection;
 
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.collections4.map.HashedMap;
 
 public class MapTest {
    public static void main(String[] args) {
 	   Map<String, Integer> map = new HashedMap<String, Integer>();
-	   map.put("1", 1);
-	   map.put("2", 2);
-	   map.put("3", 3);
 	   
-	   //换key或者value都行
+	   Map<String, Integer> linkmap = new LinkedHashMap<String, Integer>();
+	   int i = 0;
+	   while(i <= 5) {
+		   map.put(String.valueOf(i), i);
+		   linkmap.put(String.valueOf(i), i);
+		   i++;
+	   }
+	   
+	   Map<String, Integer> trMap = new TreeMap<String, Integer>(map);
+	   
+	 /*  //换key或者value都行
 	   for(String key:map.keySet()) {
 		   System.out.println(map.get(key));
 	   }
@@ -26,6 +35,9 @@ public class MapTest {
 	   while(iterator.hasNext()) {
 		   Map.Entry<String, Integer> entry = iterator.next();
 		   System.out.println(entry.getKey()+";"+entry.getValue());
-	   }
+	   }*/
+	   System.out.println(map);
+	   System.out.println(linkmap);
+	   System.out.println(trMap);
    }
 }
