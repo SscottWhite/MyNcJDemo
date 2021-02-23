fc9fcc03a2c5d0b6574afc16cc226a8d4c4239fc
god <18762988789@163.com>
2021-02-23 11:32:12 +0800

    up 2.23 1

--- src/main/java/com/example/base/interFace.java
+++ src/main/java/com/example/base/interFace.java
@@ -1,5 +1,13 @@
 package com.example.base;
 
+/**
+ * 接口特性
+	接口中每一个方法也是隐式抽象的,接口中的方法会被隐式的指定为 public abstract（只能是 public abstract，其他修饰符都会报错）。
+	接口中可以含有变量，但是接口中的变量会被隐式的指定为 public static final 变量（并且只能是 public，用 private 修饰会报编译错误）。
+	接口中的方法是不能在接口中实现的，只能由实现接口的类来实现接口中的方法。
+ * @author KJS_352
+ *
+ */
 @FunctionalInterface
 public interface interFace {
 	 void run();
--- src/main/java/com/example/collection/ListTest.java
+++ src/main/java/com/example/collection/ListTest.java
@@ -3,12 +3,38 @@ package com.example.collection;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
-
+/**
+ *  一边遍历, 一边删除
+ * @author KJS_352
+ *
+ */
 public class ListTest {
 	public static void main(String[] args) {
-		List<String> list = new ArrayList<String>();
-		Collection collections = new ArrayList();
+		List<Integer> list = new ArrayList<Integer>();		
+		
+		int i = 0;
+		while(i <= 5) {
+			list.add(i);
+			i++;
+		}
+		Iterator<Integer> it = list.iterator();
+		while(it.hasNext()) {
+			int x = it.next();
+			if(x == 3) {
+				it.remove();
+			}
+		}
+		//1.8  和上面的一样
+		list.removeIf(filter -> 2 == filter);
+		
+		/* java.util.ConcurrentModificationException    报错
+		for(Integer x:list) {
+			if(x == 3) list.remove(x);
+		}*/
+		
+		System.out.print(list);
 	}
 }
